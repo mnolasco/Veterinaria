06c7646a07ad6063f0391cd8f7975c822033bc74
mnolasco <mnolasco@SNBDMNOLASCO.auna.pvt>
2015-05-20 15:42:28 -0500

    configuracion inicial del logeo

--- CapaPresentacion/src/com/siveet/presentacion/controlador/LoginBean.java
+++ CapaPresentacion/src/com/siveet/presentacion/controlador/LoginBean.java
@@ -1,4 +1,23 @@
 package com.siveet.presentacion.controlador;
 
 public class LoginBean {
+  
+  private String alias;
+  private String clave;
+
+  public void setAlias(String alias) {
+    this.alias = alias;
+  }
+
+  public String getAlias() {
+    return alias;
+  }
+
+  public void setClave(String clave) {
+    this.clave = clave;
+  }
+
+  public String getClave() {
+    return clave;
+  }
 }
